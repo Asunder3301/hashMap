b65bd371804d140b5763a9a65cb405591232af1f
asunder <trashemail3301@gmail.com>
2026-07-13 18:25:33 -0400

    Move modulo outside the loop
    Ensures math is not truncated before the string
    is evaluated, which can lead to collisions.

--- src/modules/hashmap.js
+++ src/modules/hashmap.js
@@ -17,10 +17,10 @@ export class HashMap {
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
 
-    return hashCode;
+    return Math.abs(hashCode) % this.capacity;
   }
 
   set(key, value) {
